946935b9447ecfb70dc1a641ed799aaabe651c7f
maharaihsan <maharaihsan@gmail.com>
2023-05-04 18:09:00 +0700

    initial commit

---
+++ start-containers.sh
@@ -0,0 +1,5 @@
+docker container start frappe-<project-name>
+docker container start redis-queue-<project-name>
+docker container start redis-socketio-<project-name>
+docker container start mariadb-<project-name>
+docker container start redis-cache-<project-name>
---
+++ stop-containers.sh
@@ -0,0 +1,5 @@
+docker container stop frappe-<project-name>
+docker container stop redis-queue-<project-name>
+docker container stop redis-socketio-<project-name>
+docker container stop mariadb-<project-name>
+docker container stop redis-cache-<project-name>
